832b8e74cabc64601b8d393ee9bc298ba84d12c4
Nataliia Taran <137941324+NatSoftKrak@users.noreply.github.com>
2023-07-13 12:54:11 +0200

    Homework

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Weather forecast!");
+
+        WeatherForecast weatherForecast1 = new WeatherForecast();
+        WeatherForecast weatherForecast2 = new WeatherForecast();
+        WeatherForecast weatherForecast3 = new WeatherForecast();
+        WeatherForecast weatherForecast4 = new WeatherForecast();
+        WeatherForecast weatherForecast5 = new WeatherForecast();
+        WeatherForecast weatherForecast6 = new WeatherForecast();
+        WeatherForecast weatherForecast7 = new WeatherForecast();
+
+        System.out.println(weatherForecast1.weatherToday1);
+        System.out.println(weatherForecast2.weatherToday2);
+        System.out.println(weatherForecast3.weatherToday3);
+        System.out.println(weatherForecast4.weatherToday4);
+        System.out.println(weatherForecast5.weatherToday5);
+        System.out.println(weatherForecast6.weatherToday6);
+        System.out.println(weatherForecast7.weatherToday7);
+
+
+    }
+}
---
+++ WeatherForecast.java
@@ -0,0 +1,10 @@
+public class WeatherForecast {
+public String weatherToday1 = " Monday 28 c, sunny";
+public String weatherToday2 = "Tuesday 27 c, partly cloudy ";
+public String weatherToday3 = "Wednesday 25 c, strong wind";
+public String weatherToday4 = "Thursday 23 c, thunderstorm";
+public String weatherToday5 = "Friday 19 c, rain with hail";
+public String weatherToday6 = "Saturday 21 c, rainy";
+public String weatherToday7 = "Sunday 23 c, sunny";
+
+}
